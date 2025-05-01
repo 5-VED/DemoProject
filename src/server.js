01ff992f43148278555b20a99c0f63cd984ecbb2
5-VED <parmarved1@gmail.com>
2025-05-02 00:59:25 +0530

    Feat: Removed clustering

--- src/server.js
+++ src/server.js
@@ -1,23 +1,10 @@
-const cluster = require('cluster');
 const os = require('os');
 const app = require('./app');
 const mongoose = require('mongoose');
-const {PORT,MONGODB_URI} = require("./Config/config")
+const { PORT, MONGODB_URI } = require("./Config/config")
 
-if(cluster.isPrimary){
-    const cpus = os.cpus().length;
-
-    for(let i=0; i<cpus;i++){
-        cluster.fork();
-    }
-    cluster.on("exit",(worker,code,signal)=>{
-       console.log(`Worker ${worker.process.pid} died (${signal || code }). Restarting... `) 
+mongoose.connect(MONGODB_URI).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Worker ${process.pid}: Server running on port ${PORT}`);
     })
-}else {
-    mongoose.connect(MONGODB_URI).then(()=>{
-        console.log(`Worker process ${process.pid} connected to MongoDB`);
-        app.listen(PORT,()=>{
-            console.log(`Worker ${process.pid}: Server running on port ${PORT}`);
-        })
-    })
-}
+})
